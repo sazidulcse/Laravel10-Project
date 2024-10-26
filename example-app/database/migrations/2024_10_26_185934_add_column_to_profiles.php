33d75cc341637b017db8938dbfd56418a2654e46
sazidul rahman <100462697+sazidulcse@users.noreply.github.com>
2024-10-27 01:10:23 +0600

    last night

---
+++ example-app/database/migrations/2024_10_26_185934_add_column_to_profiles.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+
+            $table->after('name',function(Blueprint $table){
+                $table->string('nickname',10)->nullable();
+                $table->string('age',20)->default('boyosbolbo na');
+            });
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            //
+        });
+    }
+};
