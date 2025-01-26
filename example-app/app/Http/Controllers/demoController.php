98071871c83be641eb7dc574f356c212788a9d81
sazidul rahman <100462697+sazidulcse@users.noreply.github.com>
2025-01-26 21:57:29 +0600

    last commit

--- example-app/app/Http/Controllers/demoController.php
+++ example-app/app/Http/Controllers/demoController.php
@@ -7,4 +7,36 @@ use Illuminate\Http\Request;
 class demoController extends Controller
 {
     //
+
+    function SessionPut(Request $request):bool{
+
+        $email=$request->email;
+        $request->session()->put('userEmail',$email);
+        return true;
+    }
+
+    function SessionPull(Request $request){
+
+       return $request->session()->pull('userEmail','defult');
+       
+    }
+
+    function SessionGet(Request $request){
+
+        return $request->session()->get('userEmail','defult');
+        
+     }
+
+     function SessionForget(Request $request):bool{
+
+        $request->session()->forget('userEmail');
+        return true;
+        
+     }
+ 
+ 
+
+
+
+
 }
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\demoController;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\HomeController;
+use App\Http\Middleware\ActionMiddleware;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/',[HomeController::class,'index'])->name('home');
+Route::get('/{code}',[HomeController::class,'index'])->name('home')
+->middleware([ActionMiddleware::class]);
 
 Route::get('/about',[MainController::class,'About']);
 
@@ -18,3 +21,10 @@ Route::get('/skill',[MainController::class,'Skill']);
 
 
 Route::get('/contact',[MainController::class,'Contact']);
+
+
+Route::get('/test/{email}',[demoController::class,'SessionPut']);
+
+Route::get('/SessionPull',[demoController::class,'SessionPull']);
+Route::get('/SessionGet',[demoController::class,'SessionGet']);
+Route::get('/SessionForget',[demoController::class,'SessionForget']);
